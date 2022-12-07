84fe1cc0720a0e4c896c84c888a1dbe09bb8f2b9
NDH24 <pvc9610@naver.com>
2022-12-07 16:31:14 +0900

    과제 완료

--- src/App.js
+++ src/App.js
@@ -1,23 +1,110 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App1.css";
+
+function Todo(props) {
+  return (
+    <div className="Todo">
+      <h3>{props.todo.title}</h3>
+      <p>{props.todo.content}</p>
+      <p>{props.todo.isDone ? "완료" : "미완료"}</p>
+      <button onClick={() => props.handleComplete(props.todo.id)}>완료</button>
+      <button onClick={() => props.handleDelete(props.todo.id)}>삭제</button>
+    </div>
+  );
+}
 
 function App() {
+  const [number, setNumber] = useState(3);
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: "리액트 숙련",
+      content: "리액트 숙련을 공부해봅시다",
+      isDone: false,
+    },
+    {
+      id: 2,
+      title: "리액트 입문",
+      content: "리액트 입문을 공부해봅시다",
+      isDone: true,
+    },
+  ]);
+
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("form submitted");
+  };
+
+  const addTodoHandler = () => {
+    const newTodo = {
+      id: number,
+      title: title,
+      content: content,
+      isDone: false,
+    };
+    setNumber(number + 1);
+
+    setTodos([...todos, newTodo]);
+  };
+
+  const completeTodoHandler = (id) => {
+    let todo = todos.filter((todo) => todo.id === id)[0];
+    console.log(todo);
+    todo.isDone = true;
+    let todoListWithoutNewTodo = todos.filter((todo) => todo.id !== id);
+    todoListWithoutNewTodo.push(todo);
+    setTodos(todoListWithoutNewTodo);
+  };
+
+  const deleteTodoHandler = (id) => {
+    const newTodoList = todos.filter((todo) => todo.id !== id);
+    setTodos(newTodoList);
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>My Todo list</h1>
       </header>
+      <div className="Todo-input">
+        <form onSubmit={handleSubmit}>
+          <label>
+            제목:
+            <input
+              type="text"
+              name="title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </label>
+          <label>
+            내용:
+            <input
+              type="text"
+              name="content"
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
+            />
+          </label>
+          <button type="submit" onClick={addTodoHandler}>
+            추가
+          </button>
+        </form>
+      </div>
+      <div className="Todos">
+        {todos.map((todo) => {
+          return (
+            <Todo
+              todo={todo}
+              key={todo.id}
+              handleDelete={deleteTodoHandler}
+              handleComplete={completeTodoHandler}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
